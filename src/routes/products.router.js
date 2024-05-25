652e06ee1f889c66c3efaed85ee3075d04494b5b
ariellalvarez <alvarez.ariel@outlook.com>
2024-05-24 21:32:22 -0300

    preEntrega01 product user y router

---
+++ src/routes/products.router.js
@@ -0,0 +1,72 @@
+import { Router } from "express";
+const router = Router();
+
+import ProductManager from '../managers/product.manager.js';
+const productManager = new ProductManager('./src/data/products.json');
+
+router.get('/', async (req, res) => {
+    try{
+
+        const {limit} = req.query;
+        console.log(limit);
+
+        const products =await productManager.getProducts()
+        res.status(200).json(products);
+
+        
+    }catch(error){
+        res.status(404).json({ message: error.message });
+        console.log(error);
+    }
+})
+
+router.get('/:idProd', async (req, res) => {
+    try {
+      const { idProd } = req.params;
+      const product = await productManager.getProductById(idProd);
+      console.log(product);
+      if (!product) res.status(404).json({ msg: "Product not found" });
+      else res.status(200).json(product);
+    } catch (error) {
+      console.log(error);
+      res.status(500).send(error.message);
+    }
+  });
+
+  router.post('/', async (req, res) => {
+    try {
+      console.log(req.body);
+      const product = req.body;
+      const newProduct = await productManager.createProduct(req.body);
+      res.status(200).json(newProduct);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+        console.log(error);
+    }
+  });
+
+  router.put("/:idProd", async (req, res) => {
+    try {
+      const { idProd } = req.params;
+      const response = await productManager.updateProduct(req.body, idProd);
+      if (!response) res.status(404).json({ msg: "Error updating product" });
+      else res.status(200).json(response);
+    } catch (error) {
+      console.log(error);
+      res.status(500).send(error.message);
+    }
+  });
+
+  router.delete("/:idProd", async (req, res) => {
+    try {
+      const { idProd } = req.params;
+      const response = await productManager.deleteProduct(idProd);
+      if (!response) res.status(404).json({ msg: "Error delete product" });
+      else res.status(200).json(response);
+    } catch (error) {
+      console.log(error);
+      res.status(500).send(error.message);
+    }
+  });
+
+  export default router;
--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-import express from 'express';
+/*import express from 'express';
 
 import userRouter from './routes/user.router.js';
 
@@ -16,3 +16,29 @@ app.use('/pets', petRouter);
 const PORT = 8080;
 
 app.listen(PORT, ()=>console.log (`server en ${PORT}`));
+*/
+
+import express from 'express';
+//import cartRouter from './routes/cart.router.js'
+import productsRouter from './routes/products.router.js';
+//import morgan from 'morgan';
+//import { __dirname } from './path.js';
+//import { errorHandler } from './middlewares/errorHandler.js';
+
+const app = express()
+
+//app.use(express.static(__dirname + '/public'))
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+//app.use(morgan('dev'))
+
+//app.use('/api/carts', cartRouter);
+app.use('/api/products', productsRouter);
+
+//app.use(errorHandler);
+
+const PORT = 8080
+
+app.listen(PORT,()=>{
+    console.log(`Listening to ${PORT}`)
+})
